ae62cba2091abd64da4f1448a1a58abb23676373
Chearaa <chearaa@googlemail.com>
2016-08-16 20:58:02 +0200

    Added AdamWathan/BootForms.

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
          * Third Party Providers
          */
         SammyK\LaravelFacebookSdk\LaravelFacebookSdkServiceProvider::class,
+        AdamWathan\BootForms\BootFormsServiceProvider::class,
 
     ],
 
@@ -208,6 +209,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
         'Facebook' => SammyK\LaravelFacebookSdk\FacebookFacade::class,
+        'BootForm' => AdamWathan\BootForms\Facades\BootForm::class,
 
     ],
 
