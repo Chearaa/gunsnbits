e8149b6c20ecf6ac096bcc6875fb52854a7588b8
Tobias Braun <braun@weiss-intermedia.de>
2016-08-18 08:30:14 +0200

    Changed login, register and password reset forms into BootForms-Style.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,30 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-lg-offset-4 col-lg-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    Login
-                </div>
-                <div class="panel-body">
-                    <a class="btn btn-block btn-primary" href="{{ $login_url }}"><i class="fa fa-facebook"></i> Facebook</a>
-
-                    <div class="div-hr-label">
-                        <hr class="hr-label">
-                        <span class="span-label">oder</span>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-offset-4 col-lg-4 col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Login
                     </div>
+                    <div class="panel-body">
+                        <a class="btn btn-block btn-primary" href="{{ $login_url }}"><i class="fa fa-facebook"></i> Facebook</a>
 
-                    {!! BootForm::open()->post()->action( route('auth.login') )->role('form') !!}
-                    {!! BootForm::email('', 'email')->placeholder('E-Mail-Adresse')->required() !!}
-                    {!! BootForm::password('', 'password')->placeholder('Passwort')->required() !!}
-                    {!! BootForm::checkbox('eingeloggt bleiben', 'remember')->class('hidden') !!}
-                    {!! BootForm::submit('<i class="fa fa-sign-in"></i> einloggen')->class('btn btn-success pull-right') !!}
-                    {!! BootForm::close() !!}
+                        <div class="div-hr-label">
+                            <hr class="hr-label">
+                            <span class="span-label">oder</span>
+                        </div>
+
+                        {!! BootForm::open()->post()->action( route('auth.login') )->role('form') !!}
+                        {!! BootForm::email('', 'email')->placeholder('E-Mail-Adresse')->required() !!}
+                        {!! BootForm::password('', 'password')->placeholder('Passwort')->required() !!}
+                        {!! BootForm::checkbox('eingeloggt bleiben', 'remember')->class('hidden') !!}
+                        {!! BootForm::submit('<i class="fa fa-sign-in"></i> einloggen')->class('btn btn-success pull-right') !!}
+                        {!! BootForm::close() !!}
+                    </div>
                 </div>
+                <a href="{{ route('auth.password.reset') }}">Passwort vergessen?</a>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,46 +2,21 @@
 
 <!-- Main Content -->
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-envelope"></i> Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-offset-4 col-lg-4 col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8 col-xs-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Passwort zurücksetzen
+                    </div>
+                    <div class="panel-body">
+                        {!! BootForm::open()->post()->action( route('auth.password.email') )->role('form') !!}
+                        {!! BootForm::email('', 'email')->placeholder('E-Mail-Adresse')->required()->helpblock('Passwort vergessen? Kein Problem!<br/><br/>Wir senden dir einen Link an deine E-Mail-Adresse.<br/>Klicke diesen einfach an und schon kannst du dein Passwort neu vergeben.') !!}
+                        {!! BootForm::submit('<i class="fa fa-envelope"></i> Link zusenden')->class('btn btn-success pull-right') !!}
+                        {!! BootForm::close() !!}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
